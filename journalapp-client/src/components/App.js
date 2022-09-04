c2b95de7fee1f9dbfc3280f13f17cc8c8293e004
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-09-04 19:53:39 +0300

    decided to use the App component as the container, deleted Journal container, added form to Login component and imported it into the App component

--- journalapp-client/src/components/App.js
+++ journalapp-client/src/components/App.js
@@ -1,9 +1,9 @@
-
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Login/>
     </div>
   );
 }
--- journalapp-client/src/components/Login.js
+++ journalapp-client/src/components/Login.js
@@ -2,7 +2,11 @@ import React, {useEffect, useState} from "react";
 function Login(){
     return (
         <div>
-           
+            <form className={"form"}>
+                <input type={text} name="username"/>
+                <input type={password} name="password"/>
+                <button type="submit">Login</button>
+            </form>
         </div>
     );
 }
