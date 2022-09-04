7cb50efc90c91dcb013c6cf093949c470e887603
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-09-04 20:13:55 +0300

    Imported routes and route from react-router-dom in the App component to enable controled component display

--- journalapp-client/src/components/App.js
+++ journalapp-client/src/components/App.js
@@ -1,9 +1,12 @@
+import {Routes, Route} from "react-router-dom"
 import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Routes>
+      <Route path="/" element={ <Login/>}/>
+      </Routes>
     </div>
   );
 }
