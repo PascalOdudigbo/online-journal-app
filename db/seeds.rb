e12eb2e6ee8abadc9f8708c346653be2eb67f5e2
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-09-09 13:16:47 +0300

    editted the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-Journal.create(title: "First Entry", body: "First entry for first user, yeyyy!", user_id: User.first.id)
-Journal.create(title: "Second Entry", body: "Second entry for first user, yeyyy!", user_id: User.first.id)
-Journal.create(title: "Third Entry", body: "Third entry for first user, yeyyy!", user_id: User.first.id)
-Journal.create(title: "First Entry", body: "First entry for first user, yeyyy!", user_id: User.second.id)
-Journal.create(title: "Second Entry", body: "Second entry for first user, yeyyy!", user_id: User.second.id)
-Journal.create(title: "Third Entry", body: "Third entry for first user, yeyyy!", user_id: User.second.id)
+user1 = User.create(username: "makk.black", email: "makkblackbookings@gmail.com", recovery_question: "what's your dream location", answer: "the elysium fields", password: "0987654321")
+user2 =User.create(username: "Jermain Cole", email: "jermaincole@gmail.com", recovery_question: "what's your favourite book", answer: "think and grow rich", password: "0987654321")
+
+Journal.create(title: "First Entry", body: "First entry for first user, yeyyy!", user_id: user1.id)
+Journal.create(title: "Second Entry", body: "Second entry for first user, yeyyy!", user_id: user1.id)
+Journal.create(title: "Third Entry", body: "Third entry for first user, yeyyy!", user_id: user1.id)
+Journal.create(title: "First Entry", body: "First entry for first user, yeyyy!", user_id: user1.id)
+Journal.create(title: "Second Entry", body: "Second entry for first user, yeyyy!", user_id: user1.id)
+Journal.create(title: "Third Entry", body: "Third entry for first user, yeyyy!", user_id: user1.id)
 
 puts "✅ Done seeding!"
